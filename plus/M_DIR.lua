d5b13712f306482ed50cea0185f7b41f7011c002
Znak Zorro <ZnakZorro@users.noreply.github.com>
2019-03-19 10:29:16 +0100

    Add files via upload

---
+++ plus/M_DIR.lua
@@ -0,0 +1,24 @@
+local moduleName = ... 
+local M = {}
+_G[moduleName] = M
+
+function M.Start(ext) 
+   local mFiles={}
+   local i=1
+   for name,size in pairs(file.list()) do
+      local mext=name:match "[^.]+$"
+      if mext==ext then 
+         mFiles[i]={}
+         mFiles[i]['name']=name
+         mFiles[i]['size']=size
+         i=i+1
+      end	
+   end
+
+   local sort_func = function( a,b ) return a.name < b.name end
+   table.sort(mFiles, sort_func)
+
+	return mFiles
+end
+
+return M
